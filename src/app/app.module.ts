ee67eccae96148bc3df245a77a95819f35f89a3d
PanchoPizza <garcia.serrano.cbtis139.1c@gmail.com>
2020-10-29 20:47:37 -0600

    app completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagesfoundComponent,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, resolveForwardRef } from '@angular/core';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styles: [
+  ]
+})
+export class PromesasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const promesa = new Promise((resolve, reject) => 
+    {
+      if(false){
+      resolve('Hola mundo');}
+      else{reject('algo salio mal'); }
+    }
+    );
+    promesa.then((mensaje)=>{
+      console.log(mensaje);
+    }
+      
+    ).catch((error)=>{
+      console.log(error);
+    });
+    console.log('xd');
+  }
+
+}
